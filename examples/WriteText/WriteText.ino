9b1971d176896704a6c93d3ca681027826f0dded
russhughes <russ@owt.com>
2018-05-11 12:20:09 -0700

    Allow font selection using #define with default to rowmans.h

--- examples/WriteText/WriteText.ino
+++ examples/WriteText/WriteText.ino
@@ -3,6 +3,12 @@
 // Simple text drawing example
 //
 
+// You can use a #define statement to select which font to use
+// if you do not select a font (or mispell the name of the font)
+// FONT_ROWMANS will be used by default. This must be done before
+// the #include "ClassBot.h" line.
+
+#define FONT_SCRIPTS
 #include "ClassBot.h"
 
 void setup()
